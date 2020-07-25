704151001bef80a3dd7e082a61e703a0ba1f6323
Deepak Nigam <deepaknigam2702@gmail.com>
2020-07-25 13:43:25 +0530

    Added thread package.

---
+++ src/main/java/com/nineone/social/thread/PriceCalculator.java
@@ -0,0 +1,5 @@
+package com.nineone.social.thread;
+
+public class PriceCalculator {
+
+}
---
+++ src/main/java/com/nineone/social/thread/PriceCalculatorThread.java
@@ -0,0 +1,10 @@
+package com.nineone.social.thread;
+
+public class PriceCalculatorThread implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+
+}
